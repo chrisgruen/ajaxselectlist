55852cd8a763897e813742dec18fc11edf59c2db
chrisgruen <cg@romonta-schach.de>
2021-06-04 06:21:36 +0200

    composer stable dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,11 +5,11 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author' => 'Sebastian Klein',
     'author_email' => 'sebastian@sebkln.de',
-    'state' => 'beta',
+    'state' => 'alpha',
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '10.0.0-10.4.99',
         ],
     ],
 ];
